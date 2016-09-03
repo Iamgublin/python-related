9184b70b99218dd31467f28a2c543fce89c8ba74
Iamgublin <403760189@qq.com>
2016-09-03 19:20:56 +0800

    9/3

---
+++ python解析关联的http/PythonApplication5/PythonApplication5.py
@@ -0,0 +1,18 @@
+﻿import urllib
+import urllib2
+import sgmllib
+from bs4 import BeautifulSoup
+class my_sgm(sgmllib.SGMLParser):
+    def unknown_starttag(self, tag, attrs):        #解析html
+        print "tag type:"+tag
+        try:
+            for attr in attrs:
+                if attr[0]=="href":
+                    print attr[0]+':'+attr[1].encode("utf-8")
+        except:
+            pass
+    def unknown_endtag(self, tag):
+        pass
+res=urllib.urlopen("http://www.freebuf.com")
+zlz_sgm=my_sgm()
+zlz_sgm.feed(res.read())
